a193c4f0d34de023ff1ca99d689a518d2db825fc
Web-Tushar <tusharimranvip895@gmail.com>
2023-12-27 03:48:49 +0600

    navber

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        navbg:'#0D0D0D',
+        white: '#fff',
+        primay: '#FFD2A4',
+        navbtn: '#1C1D28'
+      },
+      maxWidth: {
+        'container': '1280px',
+      },
+      fontFamily: {
+        'RobotoCondensed':  "'Roboto Condensed', sans-serif",
+        
+      }
+    }
+  }
+}
