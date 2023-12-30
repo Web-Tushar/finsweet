335aca66fb24a7bce240c9d8b1eded46bdec23f1
Web-Tushar <tusharimranvip895@gmail.com>
2023-12-30 17:17:43 +0600

    upcoming section add

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ tailwind.config = {
         navbtn: '#1C1D28',
         black: '#161722',
         light_oragne:'#FFF5EB',
-        watch_bg: '#F5F2F0'
+        watch_bg: '#F5F2F0',
+        upcomungbg: 'linear-gradient(110deg, #A54E2B -53.83%, #DC9853 62.61%);'
       },
       maxWidth: {
         'container': '1280px',
@@ -20,7 +21,8 @@ tailwind.config = {
       },
       backgroundImage: {
         'banner': "url('./images/banner.jpg')",
-        overlay: ' linear-gradient(92deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%);'
+        overlay: ' linear-gradient(92deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%);',
+        
         
       }
     }
